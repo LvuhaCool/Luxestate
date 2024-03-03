b43992641aa54013801300ecf8d8f21476c8cb94
LvuhaCool <you@example.com>
2024-03-03 20:51:27 +0300

    My second commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let stbl1 = document.getElementById ("stepbl1");
+let stbrdr1 = document.getElementById ("stepbrdr1");
+
+stbl1.onmousemove = function () {
+    stbrdr1.style.borderBottom = "2px solid #fff";
+};
+stbl1.onmouseout = function () {
+    stbrdr1.style.borderBottom = "2px solid #000";
+};
+
+
+let stbl2 = document.getElementById ("stepbl2");
+let stbrdr2 = document.getElementById ("stepbrdr2");
+
+stbl2.onmousemove = function () {
+    stbrdr2.style.borderBottom = "2px solid #fff";
+};
+stbl2.onmouseout = function () {
+    stbrdr2.style.borderBottom = "2px solid #000";
+};
+
+
+let stbl3 = document.getElementById ("stepbl3");
+let stbrdr3 = document.getElementById ("stepbrdr3");
+
+stbl3.onmousemove = function () {
+    stbrdr3.style.borderBottom = "2px solid #fff";
+};
+stbl3.onmouseout = function () {
+    stbrdr3.style.borderBottom = "2px solid #000";
+};
